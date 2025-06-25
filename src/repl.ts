ccdca33bee6721c335a493122bdffc10a1e8db54
Alihan Çelikcan <alihancelikcan@gmail.com>
2025-06-25 12:38:43 +0300

    Create REPL loop

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,19 @@
 export function cleanInput(input: string): string[] {
     return input.trim().split(" ").filter(x => x.length > 0).map(x => x.toLowerCase())
 }
+
+import { createInterface } from 'node:readline';
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+});
+
+export function startREPL() {
+    rl.prompt();
+    rl.on("line", input => {
+        const inputArr = cleanInput(input);
+        console.log(`Your command was: ${inputArr[0]}`);
+        rl.prompt();
+    });
+}
